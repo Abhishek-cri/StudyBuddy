e4de1fa77760281f697f15f8086726dddad253af
Abhishek Kumar Prajapati <56184705+Abhishek-cri@users.noreply.github.com>
2023-02-28 10:14:19 +0530

    Update settings.py

--- connecthub/settings.py
+++ connecthub/settings.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+# import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -156,4 +156,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
